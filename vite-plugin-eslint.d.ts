12cb02744fe7c2d7b2d2e1116848a2284e68c72e
Adrián Steinsleger <asteins@gmail.com>
2026-03-17 13:45:47 -0300

    fix: fix lint issue

--- vite-plugin-eslint.d.ts
+++ vite-plugin-eslint.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 declare module 'vite-plugin-eslint' {
   import type { Plugin } from 'vite'
   export default function eslint(options?: Record<string, unknown>): Plugin
