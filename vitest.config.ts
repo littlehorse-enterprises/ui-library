00e8a8f6afbeaf3c15603196c4eec15538f8f0d4
Adrián Steinsleger <asteins@gmail.com>
2026-06-02 18:10:03 -0300

    fix: upgrade vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 import react from '@vitejs/plugin-react'
+import { playwright } from '@vitest/browser-playwright'
 import { defineConfig } from 'vitest/config'
 
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
@@ -10,6 +11,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(dirname, './src') } },
   test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.{test,spec}.{js,jsx,ts,tsx}',
+        'src/**/*.d.ts',
+        'src/index.ts',
+        'src/**/*.stories.{js,jsx,ts,tsx}',
+      ],
+    },
     projects: [
       {
         extends: true,
@@ -19,16 +30,6 @@ export default defineConfig({
           globals: true,
           setupFiles: ['./vitest.setup.ts'],
           include: ['src/**/*.{test,spec}.{js,mjs,cjs2,ts,mts,cts,jsx,tsx}'],
-          coverage: {
-            reporter: ['text', 'json', 'html'],
-            include: ['src/**/*.{js,jsx,ts,tsx}'],
-            exclude: [
-              'src/**/*.{test,spec}.{js,jsx,ts,tsx}',
-              'src/**/*.d.ts',
-              'src/**/index.{js,ts}',
-              'src/**/*.stories.{js,jsx,ts,tsx}',
-            ],
-          },
           typecheck: { tsconfig: './tsconfig.test.json' },
         },
       },
@@ -40,7 +41,7 @@ export default defineConfig({
           browser: {
             enabled: true,
             headless: true,
-            provider: 'playwright',
+            provider: playwright(),
             instances: [{ browser: 'chromium' }],
           },
         },
